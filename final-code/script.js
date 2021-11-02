8f6380da5c65ea30475950e4b349a2d1b3e794e8
brian_andela <brian_andela@medable.com>
2021-11-02 23:20:24 +0300

    have js to set svg

--- final-code/script.js
+++ final-code/script.js
@@ -1,3 +1,22 @@
+var planetEnum = {
+    index: 'index',
+    venus: 'venus',
+    earth: 'earth',
+    mars: 'mars',
+    jupiter: 'jupiter',
+    uranus: 'uranus',
+    neptune: 'neptune'
+}
 
+const pathname = window.location.pathname.split("/").pop()
 
-window.location.href.includes('index')
+const planet = pathname ? (pathname.split('.')[0] === 'index' ? 'mercury': pathname.split('.')[0]) : 'mercury'
+
+const planetImage = document.getElementsByClassName('planet-image')[0]
+planetImage.style.backgroundImage = `url('./assets/planet-${planet}.svg')`
+
+//if (window.location.href.includes('index')) {
+    // console.log('Let\'s go')
+    // const planetImage = document.getElementsByClassName('planet-image')
+    // planetImage.style.backgroundImage = `url(./assets/{})`
+//}
