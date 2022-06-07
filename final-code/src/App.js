489d84159f602ae3d40267cada905bfdd47a7efd
brian_andela <brian_andela@medable.com>
2022-06-07 14:01:36 +0300

    more css changes on main navigation

--- final-code/src/App.js
+++ final-code/src/App.js
@@ -5,34 +5,41 @@ import {
   Route,
   Navigate
 } from 'react-router-dom';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [show, setShow] = useState()
   return (
-     
-      <Router>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <div className='App'>
-                <MainNavigation />
-              </div>
-            }
-          >
-            {/* <Navigate to='/earth' /> */}
-          </Route>
-          {/* <Route
-          path={`/earth`}
-          exact
-          element={
-            <div className='App'>
-              <MainNavigation />
-            </div>
-          }
-        /> */}
-          <Route path='/:param' element={<div>No</div>} />
-        </Routes>
-      </Router>
+    <div className='container'>
+      <nav className='main-navigation'>
+        <div className='planets-title'>The Planets</div>
+        <div className='main-menu'>
+          <div>
+            <img src='../assets/icon-hamburger.svg' />
+          </div>
+        </div>
+        {/* inner div show, probably should show or disapper on click */}
+        <div className='planet-list'>
+          <div>Mercury</div>
+          <div>Venus</div>
+          <div>Earth</div>
+          <div>Mars</div>
+          <div>Mercury</div>
+          <div>Venus</div>
+          <div>Earth</div>
+          <div>Mars</div>
+        </div>
+      </nav>
+      <div className='planet-sub-menu'>
+        <div className='overview active-sub-menu'>Overview</div>
+        <div className='structure'>Structure</div>
+        <div className='surface'>Surface</div>
+      </div>
+      <div className='planet-picture'>pic</div>
+      <div className='planet-description'>description</div>
+      <div className='planet-footer'>footer</div>
+    </div>
   );
 }
 
